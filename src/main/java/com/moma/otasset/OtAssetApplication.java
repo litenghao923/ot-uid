0a7eb2cd62d849eca5c2371e34682f1c5b317f1a
litenghao <litenghao923@qq.com>
2019-08-20 10:16:02 +0800

    Initial commit

---
+++ src/main/java/com/moma/otasset/OtAssetApplication.java
@@ -0,0 +1,13 @@
+package com.moma.otasset;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OtAssetApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OtAssetApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/moma/otasset/OtAssetApplicationTests.java
@@ -0,0 +1,16 @@
+package com.moma.otasset;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class OtAssetApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
